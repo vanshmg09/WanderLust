a07a04f130c326055bc9b72aed5310ac2c87ba60
Vansh Maheshgawli <vmgawli95@gmail.com>
2026-06-02 17:04:15 +0530

    Reviews

--- public/js/map.js
+++ public/js/map.js
@@ -3,3 +3,16 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// If server injected coordinates for this listing, add a marker
+try{
+    if(typeof listingCoords !== 'undefined' && listingCoords){
+        // listingCoords expected in [lng, lat]
+        const lat = listingCoords[1];
+        const lng = listingCoords[0];
+        map.setView([lat, lng], 13);
+        L.marker([lat, lng]).addTo(map);
+    }
+} catch(e){
+    // ignore if listingCoords not defined in other pages
+}
